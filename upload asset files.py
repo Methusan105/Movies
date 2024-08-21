be1a001681f556bf7caa9994489b7a8f0438bea7
Methusan105 <methusan@hotmail.no>
2024-08-21 16:25:06 +0200

    1212

--- upload asset files.py
+++ upload asset files.py
@@ -1,13 +1,13 @@
 import os
 
-release_file_path = r"C:\Users\Methusan\Downloads\Aranmanai.4.zip"
+release_file_path = r"C:\Downloads\The.Warriorr.zip"
 
 # Assuming you want to iterate from 2 to 29
-for i in range(1, 9):
+for i in range(1, 6):
     file_number = f"{i:03d}"  # Format the number with leading zeros
     file_path = f'{release_file_path}.{file_number}'
     
-    command = f'gh release upload AM4 "{file_path}" --clobber'
+    command = f'gh release upload TW "{file_path}" --clobber'
     
     # Execute the command
     os.system(command)
